8c074f18118fa0cc304b7b6b92cb1deb60b35612
Nastya <himeriya97@mgmail.com>
2022-12-19 17:58:41 +0300

    fix: in  class Card add properties instead getters

--- src/components/card/card.ts
+++ src/components/card/card.ts
@@ -1,41 +1,33 @@
+import './card.scss';
 import { Data } from '../../common/types/data';
 
 export class Card {
-  public readonly data: Data;
+  private readonly data: Data;
+  private readonly brand: Data['brand'];
+  private readonly category: Data['category'];
+  // private readonly description: Data['description'];
+  private readonly discountPercentage: Data['discountPercentage'];
+  // private readonly id: Data['id'];
+  // private readonly images: Data['images'];
+  private readonly price: Data['price'];
+  private readonly rating: Data['rating'];
+  private readonly stock: Data['stock'];
+  private readonly thumbnail: Data['thumbnail'];
+  private readonly title: Data['title'];
 
   constructor(data: Data) {
     this.data = data;
-  }
-
-  get brand(): string {
-    return this.data.brand;
-  }
-  get description(): string {
-    return this.data.description;
-  }
-  get discountPercentage(): number {
-    return this.data.discountPercentage;
-  }
-  get id(): number {
-    return this.data.id;
-  }
-  get images(): string[] {
-    return this.data.images;
-  }
-  get price(): number {
-    return this.data.price;
-  }
-  get rating(): number {
-    return this.data.rating;
-  }
-  get stock(): number {
-    return this.data.stock;
-  }
-  get thumbnail(): string {
-    return this.data.thumbnail;
-  }
-  get title(): string {
-    return this.data.title;
+    this.brand = this.data.brand;
+    this.category = this.data.category;
+    // this.description = this.data.description;
+    this.discountPercentage = this.data.discountPercentage;
+    // this.id = this.data.id;
+    // this.images = this.data.images;
+    this.price = this.data.price;
+    this.rating = this.data.rating;
+    this.stock = this.data.stock;
+    this.thumbnail = this.data.thumbnail;
+    this.title = this.data.title;
   }
 
   // Метод только возвращает отрисованную карточку, никуда ее пока не добавляет
@@ -46,7 +38,7 @@ export class Card {
     const cardClone = cardTemplate.content.cloneNode(true) as HTMLElement;
 
     // Чтобы название вмещалось в одну строку удаляем лишние символы, если они есть, и добавляем многоточие
-    let title: string = this.data.title;
+    let title: string = this.title;
     if (title.length > 20) {
       title = `${title.slice(0, 20)}...`;
     }
@@ -55,29 +47,40 @@ export class Card {
     cardTitle.innerText = title;
 
     const cardImg = cardClone.querySelector('.card__img') as HTMLElement;
-    cardImg.style.backgroundImage = `url(${this.data.thumbnail})`;
+    cardImg.style.backgroundImage = `url(${this.thumbnail})`;
+
+    // cardImg.addEventListener('click', () => { changePage(e) })              //листнер перехода на др страницу
 
     const cardCategory = cardClone.querySelector(
       '.card__category',
     ) as HTMLElement;
-    cardCategory.innerText = `Category: ${this.data.category}`;
+    cardCategory.innerText = `Category: ${this.category}`;
 
     const cardBrand = cardClone.querySelector('.card__brand') as HTMLElement;
-    cardBrand.innerText = `Brand: ${this.data.brand}`;
+    cardBrand.innerText = `Brand: ${this.brand}`;
 
     const cardPrice = cardClone.querySelector('.card__price') as HTMLElement;
-    cardPrice.innerText = `$${this.data.price}`;
+    cardPrice.innerText = `$${this.price}`;
 
     const cardDiscount = cardClone.querySelector(
       '.card__discount',
     ) as HTMLElement;
-    cardDiscount.innerText = `-${this.data.discountPercentage}%`;
+    cardDiscount.innerText = `-${this.discountPercentage}%`;
 
     const cardRating = cardClone.querySelector('.card__rating') as HTMLElement;
-    cardRating.innerText = `${this.data.rating}`;
+    cardRating.innerText = `${this.rating}`;
 
     const cardStock = cardClone.querySelector('.card__stock') as HTMLElement;
-    cardStock.innerText = `left: ${this.data.stock}`;
+    cardStock.innerText = `left: ${this.stock}`;
+
+    // может можно в эти листнеры передать сразу this.data?
+
+    // const addBtn = new Button('', () => { addToCart(e) });                 //листнер добавленя в корзину
+    // addBtn.classList.add('add-to-cart');                                   //особые стили для кнопки добавления в корзину
+    // const detailsBtn = new Button('Details', () => { changePage(e) });     //листнер перехода на др страницу
+
+    // const cardBtns = cardClone.querySelector('.card__buttons') as HTMLElement;
+    // cardBtns.append(addBtn, detailsBtn);                                   //добавляем кнопки к шаблону
 
     return cardClone;
   }
