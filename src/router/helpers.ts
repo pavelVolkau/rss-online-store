87adabc2a7e1d23280a5c2cdf5793af503722157
Nastya <himeriya97@mgmail.com>
2022-12-25 12:24:17 +0300

    feat: add reading of filter query params for render

---
+++ src/router/helpers.ts
@@ -0,0 +1,131 @@
+import { Data } from '../common/types/data';
+
+const QueryParams = {
+  category: 'category',
+  brand: 'brand',
+  price: 'price',
+  stock: 'stock',
+  sort: 'sort',
+  search: 'search',
+  size: 'size',
+};
+
+export function createLink(location: HTMLLinkElement | Location) {
+  return new URL(location.href).pathname.concat(
+    new URL(location.href).hash,
+    new URL(location.href).search,
+  );
+}
+
+export function elemInQuery(query: string, element: Data): boolean {
+  let category = true;
+  let brand = true;
+  let price = true;
+  let stock = true;
+  let search = true;
+  // если параметр есть в квери, то мы проверяем содержит ли его элемент, если нет, то остается true, чтобы он пропускался
+  if (queryHasParam(query, QueryParams.category)) {
+    category = elemContainsQueryCategoryBrand(
+      query,
+      QueryParams.category,
+      element,
+    );
+  }
+  if (queryHasParam(query, QueryParams.brand)) {
+    brand = elemContainsQueryCategoryBrand(query, QueryParams.brand, element);
+  }
+  if (queryHasParam(query, QueryParams.price)) {
+    price = elemContainsQueryPriceStock(query, QueryParams.price, element);
+  }
+  if (queryHasParam(query, QueryParams.stock)) {
+    stock = elemContainsQueryPriceStock(query, QueryParams.stock, element);
+  }
+  if (queryHasParam(query, QueryParams.search)) {
+    search = elemContainsQuerySearch(query, QueryParams.search, element);
+  }
+  return category && brand && price && stock && search;
+}
+
+function queryHasParam(query: string, param: string): boolean {
+  const queryArr = query.split('&');
+  let result = false;
+  queryArr.forEach((el: string) => {
+    if (el.split('=')[0] === param) {
+      result = true;
+    }
+  });
+  return result;
+}
+
+//для текстовых параметров category, brand
+function elemContainsQueryCategoryBrand(
+  query: string,
+  param: string,
+  element: Data,
+): boolean {
+  const queryArr = query.split('&'); // вид ссылки будет такой category=laptops%fragrances&brand=Samsung&price=600%1400...
+  let result = false;
+  queryArr.forEach((el: string) => {
+    if (el.split('=')[0] === param) {
+      el.split('=')[1]
+        .split('%') //Этим значком будем делить разные фильтры внутри категории,н.п. category=laptops%fragrances%...
+        .forEach((val: string) => {
+          if (val === String(element[param as keyof Data]).toLowerCase()) {
+            result = true;
+          }
+        });
+    }
+  });
+  return result;
+}
+
+//для числовых параметров price, stock
+function elemContainsQueryPriceStock(
+  query: string,
+  param: string,
+  element: Data,
+): boolean {
+  const queryArr = query.split('&'); // вид ссылки будет такой category=laptops%fragrances&brand=Samsung&price=600%1400...
+  let result = false;
+  queryArr.forEach((el: string) => {
+    if (el.split('=')[0] === param) {
+      const min = Number(el.split('=')[1].split('%')[0]);
+      const max = Number(el.split('=')[1].split('%')[1]);
+      if (
+        element[param as keyof Data] >= min &&
+        element[param as keyof Data] <= max
+      ) {
+        result = true;
+      }
+    }
+  });
+  return result;
+}
+
+//для поиска search
+function elemContainsQuerySearch(
+  query: string,
+  param: string,
+  element: Data,
+): boolean {
+  const queryArr = query.split('&'); // вид ссылки будет такой category=laptops%fragrances&brand=Samsung&price=600%1400...
+  let result = false;
+  queryArr.forEach((el: string) => {
+    if (el.split('=')[0] === param) {
+      const searchString = el.split('=')[1].split('%').join(' ').toLowerCase();
+      if (
+        element.brand.toLowerCase().includes(searchString) ||
+        element.category.toLowerCase().includes(searchString) ||
+        element.description.toLowerCase().includes(searchString) ||
+        String(element.discountPercentage).includes(searchString) ||
+        String(element.price).includes(searchString) ||
+        String(element.rating).includes(searchString) ||
+        String(element.stock).includes(searchString) ||
+        element.title.toLowerCase().includes(searchString)
+      ) {
+        result = true;
+      }
+    }
+  });
+  return result;
+}
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,7 +1,9 @@
 import { LINK, ROUTES } from '../common/helpers/constants';
 import { Data } from '../common/types/data';
-import { Card } from '../components/card/card';
-import { DataLoader } from '../components/data-loader/dataLoader';
+import { Card } from '../components/card/Card';
+import { DataLoader } from '../components/data-loader/DataLoader';
+import { GoodsBox } from '../components/goods-box/GoodsBox';
+import { createLink, elemInQuery } from './helpers';
 
 const loader = new DataLoader(LINK);
 // TODO: импортировать шаблоны страниц
@@ -15,46 +17,49 @@ export const routes = {
 //принимает путь и заменяет контент в html в диве app-root
 export function render(path: string): void {
   const appRoot = document.querySelector('.app-root') as HTMLElement;
-  appRoot.innerHTML = '';
-  // let result = '<h1>404 Not found</h1>'; //этот тег покажется если страницы с таким путем не существует
-  if (routes.Main.match(path)) {
+  // appRoot.innerHTML = '';
+  if (routes.Main.match(path.split('?')[0])) {
     //сравниваем совпадает ли переданный путь с путем к главной страницы
     // TODO: result = шаблон главной страницы
-    // result = '<h1>Main page</h1>';
-    loader.getData((data: Data[]) => {
-      data.forEach((el: Data) => {
-        appRoot.appendChild(new Card(el).draw());
+    if (path.split('?').length === 1) {
+      loader.getData((data: Data[]) => {
+        appRoot.replaceChildren(new GoodsBox(data).draw());
       });
-    });
-  } else if (routes.Details.match('/' + path.split('/')[1].split('#')[0])) {
+    } else {
+      const queries = path.split('?')[1]; //отрезаем знак вопроса
+      loader.getData((data: Data[]) => {
+        const newData = data.filter((elem) => {
+          return elemInQuery(queries, elem);
+        });
+        appRoot.replaceChildren(new GoodsBox(newData).draw());
+      });
+    }
+  } else if (routes.Details.match(path.split('#')[0])) {
     //TODO: Подумать как это можно привести к норм виду
     // TODO: result = шаблон страницы деталей
-    console.log(path);
     if (Number(path.split('#')[1]) > 0 && Number(path.split('#')[1]) < 101) {
-      console.log('success');
-      // result = '<h1>Card</h1>';
       loader.getData((data: Data[]) => {
         data.forEach((el: Data) => {
           if (el.id === Number(path.split('#')[1])) {
-            appRoot.appendChild(new Card(el).draw());
+            appRoot.replaceChildren(new Card(el).draw());
           }
         });
       });
+    } else {
+      appRoot.replaceChildren('<h1>Not found</h1>');
     }
   } else if (routes.Cart.match(path)) {
     // TODO: result = шаблон страницы с корзиной
-    // result = '<h1>Cart page</h1>';
+    appRoot.replaceChildren('<h1>Cart </h1>');
+  } else {
+    appRoot.replaceChildren('<h1>Not found</h1>');
   }
-
-  // добавляем контент на страницу
-  // appRoot.innerHTML = result;
 }
 
 //функция для перехода на переданную страницу, все переходы по ссылкам должны происходить через эту функцию, иначе не будет писаться история в браузер
 export function goTo(path: string): void {
   window.history.pushState({ path }, path, path); //добавляет путь в историю браузера, чтобы можно было потом переключать страницы стрелками в браузере
   render(path);
-  console.log(window.location);
 }
 
 //функция инициализации роутинга
@@ -62,33 +67,15 @@ export function initRouter(): void {
   // событие, кот вызывается при переходе по истории браузера
   //тут мы получается когда нажимаем на кнопки в браузере должна рендериться страница с url на который мы переходим
   window.addEventListener('popstate', () => {
-    render(
-      new URL(window.location.href).pathname.concat(
-        new URL(window.location.href).hash,
-      ),
-    );
-    console.log(window.location);
-    //window.location.href это полная ссылка, н.п. https://www.google.com/search?q=%....
-    //new URL это объект, pathname возвращает путь после корневой директории, в случае с верхней ссылкой это /search
-    //в нашем случае должно возвращаться либо '/' либо '/details' либо '/cart'
+    render(createLink(window.location));
   });
   //ищем все ссылки, которые ведут не в корень сайта, чтобы поставить им функцию goTo вместо дефолтного перехода
   document.querySelectorAll('[href^="/"]').forEach((el) => {
     el.addEventListener('click', (e) => {
       e.preventDefault();
       const target = e.target as HTMLLinkElement;
-      const path = new URL(target.href).pathname.concat(
-        new URL(target.href).hash,
-      );
-      goTo(path);
+      goTo(createLink(target));
     });
   });
-  console.log(new URL(window.location.href));
-  render(
-    new URL(window.location.href).pathname.concat(
-      new URL(window.location.href).hash,
-    ),
-  ); //этот рендер нужен для того, чтобы если нажать обновить страницу она обновилась правильно
+  render(createLink(window.location)); //этот рендер нужен для того, чтобы если нажать обновить страницу она обновилась правильно
 }
-
-//TODO: подключить роутер в app.ts и вызвать initRouter
