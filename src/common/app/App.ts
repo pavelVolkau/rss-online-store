5df435259cd5d8b4253cd326b4a9b478a805bff6
pavelVolkau <lekoo.cwalk@gmail.com>
2022-12-30 15:51:45 +0300

    fix: resolve merge conflicts fixed base styles, correctly positioned the main blocks of the page

--- src/common/app/App.ts
+++ src/common/app/App.ts
@@ -1,7 +1,10 @@
-import './app-root.scss';
+import './app.scss';
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
 import { initRouter } from '../../router/router';
+import getDOMElement from '../helpers/getDOMElement';
+import { TAGS } from '../helpers/constants';
+import CONSTANTS from './constants';
 // import { LINK } from '../helpers/constants';
 // import { Data } from '../types/data';
 
@@ -12,10 +15,14 @@ export default class App {
   private footer = new Footer().draw() as HTMLElement;
 
   start(): void {
-    initRouter();
-    const appRoot = document.querySelector('.app-root') as HTMLElement;
+    const BODY = document.body;
+    const main = getDOMElement(TAGS.main, CONSTANTS.main.class);
+    const appRoot = getDOMElement(TAGS.div, CONSTANTS.appRoot.class);
 
-    appRoot.append(this.header, this.footer);
+    main.append(appRoot);
+
+    BODY.append(this.header, main, this.footer);
+    initRouter();
   }
 }
 
