afdfaa403bbc828517d8db0ad8fc23308c441197
pavelVolkau <lekoo.cwalk@gmail.com>
2023-01-06 23:39:32 +0300

    feat: added function getNewLinkWithQuery to create a link when the query string changes

--- src/common/helpers/getQueryURI.ts
+++ src/common/helpers/getQueryURI.ts
@@ -1,8 +1,7 @@
 import { SEPARATORS } from './constants';
 
-export default function getQueryURI(): string {
-  const link = window.location.href;
-  const query = link.split(SEPARATORS.searchQuery)[1];
+export default function getQueryURI(href: string): string {
+  const query = href.split(SEPARATORS.searchQuery)[1];
 
   if (query) return query;
 
